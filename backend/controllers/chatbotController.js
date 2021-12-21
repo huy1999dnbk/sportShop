357c768668f962b50ced27f3e66680d7125d7be7
HUY PHAM <huy.pham@sapotacorp.vn>
2021-12-21 16:58:31 +0700

    add check nodeenv and deploy

--- backend/controllers/chatbotController.js
+++ backend/controllers/chatbotController.js
@@ -1,8 +1,8 @@
 import asyncHandler from 'express-async-handler'
 import dialogFlow from '@google-cloud/dialogflow'
 import dotenv from 'dotenv'
-import {v4 as uuidv4} from 'uuid'
-import {dialogFlowSessionLanguageCode } from '../config/keys.js'
+import { v4 as uuidv4 } from 'uuid'
+import { dialogFlowSessionLanguageCode } from '../config/keys.js'
 dotenv.config()
 
 const projectId = process.env.GOOGLE_PROJECT_ID
@@ -11,19 +11,19 @@ const google_client_email = process.env.GOOGLE_CLIENT_EMAIL
 
 const credentials = {
     client_email: google_client_email.toString(),
-    private_key:process.env.GOOGLE_PRIVATE_KEY,
-  };
+    private_key: process.env.NODE_ENV === 'production' ? JSON.parse(process.env.GOOGLE_PRIVATE_KEY) : process.env.GOOGLE_PRIVATE_KEY,
+};
 
 
 const sessionId = process.env.DIALOGFLOW_SESSION_ID
-const sessionClient = new dialogFlow.SessionsClient({ projectId:projectId.toString(),credentials})
+const sessionClient = new dialogFlow.SessionsClient({ projectId: projectId.toString(), credentials })
 const sessionPath = sessionClient.projectAgentSessionPath(
     projectId.toString(),
     sessionId
 )
 
 
-const sendQuery = asyncHandler(async(req, res) => {
+const sendQuery = asyncHandler(async (req, res) => {
     const request = {
         session: sessionPath,
         queryInput: {
@@ -39,11 +39,11 @@ const sendQuery = asyncHandler(async(req, res) => {
     const responses = await sessionClient.detectIntent(request);
     const result = responses[0].queryResult;
     res.json({
-        'result':result
+        'result': result
     })
 })
 
-const sendEvent = asyncHandler(async(req, res) => {
+const sendEvent = asyncHandler(async (req, res) => {
     const request = {
         session: sessionPath,
         queryInput: {
@@ -59,10 +59,10 @@ const sendEvent = asyncHandler(async(req, res) => {
     const responses = await sessionClient.detectIntent(request);
     const result = responses[0].queryResult;
     res.json({
-        'result':result
+        'result': result
     })
 })
 
 export {
-    sendQuery,sendEvent
+    sendQuery, sendEvent
 }
