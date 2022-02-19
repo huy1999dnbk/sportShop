acbe36d966342cf46f2297f9fb3df61987ee364b
HUY PHAM <huy.pham@sapotacorp.vn>
2022-02-19 10:50:42 +0700

    fix item paginate

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -4,7 +4,7 @@ const Order = require('../model/orderModel')
 const stringSimilarity = require("string-similarity");
 
 const getProducts = asyncHandler(async (req, res) => {
-  const pageSize = 6
+  const pageSize = 10
   const page = Number(req.query.pageNumber) || 1
   const keyword = req.query.keyword ? {
     name: {
